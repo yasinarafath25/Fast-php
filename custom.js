8b51caebcc0fecb66e29836a1ee392cf5555553d
yasinarafath25 <mdyasinarafath2004@gmail.com>
2025-05-28 01:38:48 +0300

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,68 @@
+/* Search Form Toggle */
+jQuery(document).ready(function() {
+	jQuery(".header-search-toggle").click(function() {
+	   jQuery(".header-search-form").toggle();
+	   jQuery(".header-search-form .search-field").focus();
+	 });
+});
+
+jQuery(document).ready(function() {
+    jQuery('#logo-main h2').html(function(_, html) {
+        const words = html.split(' ');
+        if (words.length > 1) {
+            words[1] = '<span>' + words[1] + '</span>';
+        }
+        return words.join(' ');
+    });
+});
+
+/*Mobile Menu Handle*/
+jQuery(document).ready(function () {
+/* MOBILE TOGLLE MENU HANDLE*/
+var menuFocus, navToggleItem, focusBackward;
+var menuToggle = document.querySelector('.menu-toggle');
+var navMenu = document.querySelector('.menu');
+var navMenuLinks = navMenu.getElementsByTagName('a');
+var navMenuListItems = navMenu.querySelectorAll('li');
+var nav_lastIndex = navMenuListItems.length - 1;
+var navLastParent = document.querySelectorAll('.main-navigation > ul > li').length - 1;
+
+document.addEventListener('menu_focusin', function () {
+    menuFocus = document.activeElement;
+    if (navToggleItem && menuFocus !== navMenuLinks[0]) {
+        document.querySelectorAll('.main-navigation > ul > li')[navLastParent].querySelector('a').focus();
+    }
+    if (menuFocus === menuToggle) {
+        navToggleItem = true;
+    } else {
+        navToggleItem = false;
+    }
+}, true);
+
+
+document.addEventListener('keydown', function (e) {
+    if (e.shiftKey && e.keyCode == 9) {
+        focusBackward = true;
+    } else {
+        focusBackward = false;
+    }
+});
+
+
+for (el of navMenuLinks) {
+    el.addEventListener('blur', function (e) {
+        if (!focusBackward) {
+            if (e.target === navMenuLinks[nav_lastIndex]) {
+                menuToggle.focus();
+            }
+        }
+    });
+}
+menuToggle.addEventListener('blur', function (e) {
+    if (focusBackward) {
+        navMenuLinks[nav_lastIndex].focus();
+    }
+});
+
+
+});
